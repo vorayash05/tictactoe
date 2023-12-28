c67c67732729b6981227df2b8bd0ef78980a6ef3
Yash Vora <vorayash05@gmail.com>
2023-12-28 18:51:47 +0530

    Implement board functions for the Tic-Tac-Toe game

--- constants.py
+++ constants.py
@@ -1,10 +1,23 @@
+#Board setup
 width = 600
 height = 600
-
-#Colors
-bg_color = (28, 170, 156)
 rows = 3
 cols = 3
 square_size = width//cols
-line_color = (23, 145, 135)
 line_width = 15
+
+
+# Circle params
+circle_width = 	15
+radius = square_size//4
+
+
+#Colors
+bg_color = (28, 170, 156)
+line_color = (23, 145, 135)
+circle_color = (239, 231, 200)
+cross_color = (66, 66, 66)
+
+#Cross params
+cross_width  = 20
+offset = 50
--- tictactoe.py
+++ tictactoe.py
@@ -15,21 +15,72 @@ screen.fill(bg_color)
 class Board:
 	def __init__(self):
 		self.squares = np.zeros((rows, cols))
-		print(self.squares)
+		self.empty_squares = self.squares  #Initialising the whole board as empty
+		self.marked_squares = 0
 
+
+	def final_state(self):
+		'''
+		return 0 if there is no win yet
+		return 1 if player 1 wins
+		return 2 if player 2 wins	
+		'''
+		
+		#vertical wins
+		for col in range(cols):
+			if self.squares[0][col] == self.squares[1][col] == self.squares[2][col] !=0:
+					return self.squares[0][col]
+
+		#horizontal wins
+		for row in range(rows):
+			if self.squares[row][0] == self.squares[row][1] == self.squares[row][2] !=0:
+					return self.squares[row][0]
+
+		#diagonal wins
+		if self.squares[0][0] == self.squares[1][1] == self.squares[2][2] !=0:
+			return self.squares[0][0]
+
+		if self.squares[0][2] == self.squares[1][1] == self.squares[2][0] !=0:
+			return self.squares[0][2]
+
+		return 0 #no win yet
+
+
+
+
+
+	
 	def mark_square(self, row, col, player):
 		self.squares[row][col] = player
+		self.marked_squares +=1
 
 	def empty_square(self, row, col):
 		return self.squares[row][col] == 0
+
+	def is_full(self):
+		return self.marked_squares == 9
+
+	def is_empty(self):
+		return self.marked_squares == 0
+
+	def get_empty(self):
+		empty_squares = []
+		for row in range(rows):
+			for col in range(cols):
+				if self.empty_square(row, col):
+					empty_squares.append((row, col))
+		return empty_squares
+
 
 
 
 class Game():
 	def __init__(self):
 		self.board = Board()
+		self.player = 1 # Player 1- X, Player 2- 0
+		self.game_mode = 'pvp' #Player v player or AI
+		self.running = True
 		self.show_lines()
-		self.player = 1
 
 	def show_lines(self):
 		#vertical lines
@@ -42,6 +93,26 @@ class Game():
 
 	def next_turn(self):
 		self.player = 3 - self.player
+
+	def draw_fig(self, row, col):
+		if self.player == 1:
+		#draw cross
+			start_desc = (col * square_size + offset, row*square_size + offset)
+			end_desc = (col * square_size + square_size - offset, row * square_size + square_size - offset)
+			pygame.draw.line(screen, cross_color, start_desc, end_desc, cross_width)
+
+			start_desc = (col * square_size + offset, row*square_size + square_size - offset)
+			end_desc = (col * square_size + square_size - offset, row * square_size + offset)
+			pygame.draw.line(screen, cross_color, start_desc, end_desc, cross_width)	
+
+
+		elif self.player == 2:
+		#draw circle
+			center = (col * square_size + square_size//2, row*square_size + square_size//2)
+			pygame.draw.circle(screen, circle_color, center, radius, circle_width)
+
+
+
 
 
 
@@ -65,6 +136,7 @@ def main():
 				
 				if board.empty_square(row, col):
 					board.mark_square(row, col, game.player)
+					game.draw_fig(row, col)
 					game.next_turn()
 					print(board.squares)
 				
